9ba1b493924130f3489887914235af33c6e88214
magusin <fanatsy4@gmail.com>
2023-05-31 19:47:40 +0200

    add function afficherCarte

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Afficher les cartes
-const couleurs = ["trèfle", "pique", "carreau", "coeur"];
-const valeurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Reine", "Roi", "As"];
-const jeuDeCartes = [];
-let jeuxComplet = document.querySelector("#jeuxComplet");
+document.addEventListener('DOMContentLoaded', function () {
+  // Afficher les cartes
+  const couleurs = ['trèfle', 'pique', 'carreau', 'coeur']
+  const valeurs = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'Valet',
+    'Reine',
+    'Roi',
+    'As'
+  ]
+  const jeuDeCartes = []
+  let jeuxComplet = document.querySelector('#jeuxComplet')
 
-for (let i = 0; i < couleurs.length; i++) {
+  for (let i = 0; i < couleurs.length; i++) {
     for (let j = 0; j < valeurs.length; j++) {
       const carte = {
         valeur: valeurs[j],
         couleur: couleurs[i]
-      };
-      jeuDeCartes.push(carte);
+      }
+      jeuDeCartes.push(carte)
     }
   }
 
-  for (let i = 0; i < jeuDeCartes.length; i++) {
-    jeuxComplet.innerHTML += `<img src=../assets/${jeuDeCartes[i].valeur}-de-${jeuDeCartes[i].couleur}.avif>`;
+  function afficherCarte(index) {
+    if (index < jeuDeCartes.length) {
+      jeuxComplet.innerHTML += `<img src="../assets/${jeuDeCartes[index].valeur}-de-${jeuDeCartes[index].couleur}.avif">`;
+      setTimeout(function() {
+        afficherCarte(index + 1);
+      }, 100); // Temps d'attente en millisecondes (ici 1 seconde)
+    }
   }
+  
+  afficherCarte(0);
 
-console.log(jeuDeCartes)
-});
+  //   for (let i = 0; i < jeuDeCartes.length; i++) {
+  //     jeuxComplet.innerHTML += `<img src=../assets/${jeuDeCartes[i].valeur}-de-${jeuDeCartes[i].couleur}.avif>`;
+  //   }
+
+  console.log(jeuDeCartes)
+})
